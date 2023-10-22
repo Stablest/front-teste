1e1066f896816d29a664a07528312e08ad9c9c06
Stablest <marlonlocci@hotmail.com>
2023-10-22 19:19:21 -0300

    feat: Removed register page

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FormRegister } from "@/components/form-register";
-
-export default function Register() {
-    return (
-        <main className="d-flex justify-content-center align-items-center vh-100">
-            <FormRegister></FormRegister>
-        </main>
-    )
-}
