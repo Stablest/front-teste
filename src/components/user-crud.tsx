3502c107893da1b3c84f5ddd93f5cc36a0cada2f
Stablest <marlonlocci@hotmail.com>
2023-10-22 12:28:03 -0300

    feat: Added open modal button

--- src/components/user-crud.tsx
+++ src/components/user-crud.tsx
@@ -1,10 +1,21 @@
+'use client'
+
+import { useState } from "react"
 import { TableUser } from "./table-user"
+import ModalCreateUser from "./modal-create-user"
 
 function UserCRUD() {
+
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+    function createButtonHandler() {
+        setIsModalOpen(!isModalOpen)
+    }
+
     return (
         <div className="px-3">
             <div className="d-flex justify-content-end align-items-center column-gap-4 border-bottom pb-2">
-                <button className="bg-black rounded text-white border-0 fs-3 d-flex justify-content-center align-items-center shadow-sm" style={{ width: '32px', height: '32px' }}>
+                <button className="bg-black rounded text-white border-0 fs-3 d-flex justify-content-center align-items-center shadow-sm" style={{ width: '32px', height: '32px' }} onClick={createButtonHandler}>
                     +
                 </button>
                 <div className="shadow-sm d-flex align-items-center">
@@ -14,6 +25,7 @@ function UserCRUD() {
                     </button>
                 </div>
             </div>
+            {isModalOpen ? <ModalCreateUser closeButtonHandler={createButtonHandler}></ModalCreateUser> : null}
             <TableUser></TableUser>
         </div>
     )
