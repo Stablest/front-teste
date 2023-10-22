26429c5ba8a3766be592d8a5b35e67e0d95b9c94
Stablest <marlonlocci@hotmail.com>
2023-10-22 20:45:40 -0300

    refactor: Updated main class

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { UserCRUD } from "@/components/user-crud";
 
 export default function Dashboard() {
     return (
-        <main>
+        <main className="overflow-auto">
             <h3 className="border-bottom border-1 border-black text-center mt-3 py-2 fw-bold">Listagem De Usuários</h3>
             <UserCRUD></UserCRUD>
         </main>
