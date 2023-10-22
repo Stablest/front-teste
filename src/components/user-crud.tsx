8329e25b7a92518b90621a1bae434d88d6d03ce2
Stablest <marlonlocci@hotmail.com>
2023-10-22 16:59:00 -0300

    feat: Added UserCRUD Component

--- src/components/user-crud.tsx
+++ src/components/user-crud.tsx
@@ -1,32 +1,84 @@
 'use client'
 
-import { useState } from "react"
+import { FormEvent, Suspense, useEffect, useState } from "react"
 import { TableUser } from "./table-user"
 import ModalCreateUser from "./modal-create-user"
+import { IUser } from "@/utils/interfaces/IUser"
+import { URL } from "@/utils/enums/URL"
 
 function UserCRUD() {
-
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+    const [isCreateUserModalOpen, setIsCreateUserModalOpen] = useState<boolean>(false)
+    const [users, setUsers] = useState<IUser[]>([])
+    const [displayedUsers, setDisplayedUsers] = useState<IUser[]>([])
 
     function createButtonHandler() {
-        setIsModalOpen(!isModalOpen)
+        setIsCreateUserModalOpen(!isCreateUserModalOpen)
     }
+
+    function inputHandler(newValue: string) {
+        const newDisplayedUsers: IUser[] = []
+        users.forEach((user, index) => {
+            if ((user.name.toLowerCase().includes(newValue.toLowerCase())) || user.cpf.includes(newValue))
+                newDisplayedUsers.push(user)
+        })
+        setDisplayedUsers(newDisplayedUsers)
+    }
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function getAllUsers() {
+            if (ignore)
+                return
+            try {
+                const loggedUserJWT = window.localStorage.getItem('jwt_token')
+                const res = await fetch(`${URL.BASE}/user/`, {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": 'application/json',
+                        "Authorization": `Bearer ${loggedUserJWT}`
+                    },
+                    credentials: "include",
+                })
+                const { message, users: loadedUsers } = await res.json()
+                if (message) {
+                    return
+                }
+                setUsers(loadedUsers)
+                setDisplayedUsers(loadedUsers)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        getAllUsers()
+
+        return () => {
+            ignore = true;
+        }
+    }, [])
+
+
 
     return (
         <div className="px-3">
-            <div className="d-flex justify-content-end align-items-center column-gap-4 border-bottom pb-2">
-                <button className="bg-black rounded text-white border-0 fs-3 d-flex justify-content-center align-items-center shadow-sm" style={{ width: '32px', height: '32px' }} onClick={createButtonHandler}>
-                    +
-                </button>
-                <div className="shadow-sm d-flex align-items-center">
-                    <input type="text" name="search-bar" className="" style={{ width: '', height: '32px' }} />
-                    <button className="bg-transparent border-0">
-                        <img src="find.svg" alt="Magnifier icon" className="bg-black rounded-end" width={'32px'} height={'32px'} />
+            <div className="d-flex justify-content-between align-items-center column-gap-4 border-bottom pb-2">
+                <button type="button" className="bg-success text-light">Exportar</button>
+                <div className="d-flex">
+                    <button className="bg-black rounded text-white border-0 fs-3 d-flex justify-content-center align-items-center shadow-sm" style={{ width: '32px', height: '32px' }} onClick={createButtonHandler}>
+                        +
                     </button>
+                    <form className="shadow-sm d-flex align-items-center">
+                        <input type="text" name="search-bar" className="" style={{ width: '', height: '32px' }} onChange={e => inputHandler(e.target.value)} />
+                        <button type="submit" className="bg-transparent border-0">
+                            <img src="find.svg" alt="Magnifier icon" className="bg-black rounded-end" width={'32px'} height={'32px'} />
+                        </button>
+                    </form>
                 </div>
             </div>
-            {isModalOpen ? <ModalCreateUser closeButtonHandler={createButtonHandler}></ModalCreateUser> : null}
-            <TableUser></TableUser>
+            {isCreateUserModalOpen ? <ModalCreateUser closeButtonHandler={createButtonHandler}></ModalCreateUser> : null}
+            <Suspense>
+                <TableUser users={displayedUsers}></TableUser>
+            </Suspense>
         </div>
     )
 }
