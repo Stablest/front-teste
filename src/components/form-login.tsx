7c52a6396deeb9bb83d338e539298f07d7582984
Stablest <marlonlocci@hotmail.com>
2023-10-22 21:05:39 -0300

    refactor: Changed email login option to login username

--- src/components/form-login.tsx
+++ src/components/form-login.tsx
@@ -10,15 +10,15 @@ function FormLogin() {
     async function formHandler(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
-        const login = {
-            email: formData.get('email'),
+        const tryLogin = {
+            login: formData.get('login'),
             password: formData.get('password')
         }
         try {
             const res = await fetch(`${URL.BASE}/user/auth`, {
                 method: 'POST',
                 headers: { "Content-Type": 'application/json' },
-                body: JSON.stringify(login),
+                body: JSON.stringify(tryLogin),
                 credentials: "include",
             })
             const { message, token, user } = await res.json()
@@ -37,11 +37,13 @@ function FormLogin() {
     return (
         <>
 
-            <form action='/dashboard' className="d-flex flex-column p-2 bg-success rounded" style={{ width: "18rem" }} onSubmit={e => formHandler(e)}>
-                <input type="email" name="email" placeholder="E-mail" required className="p-2 m-2 rounded border-0 bg-light" />
-                <input type="password" name="password" placeholder="Senha" required className="p-2 m-2 rounded border-0 bg-light" />
-                {errorMessage ? <p className="text-danger fw-bold">{errorMessage}</p> : null}
-                <button type="submit" className="mx-auto mt-4 rounded border-0 text-light bg-success fs-5 fw-bold rounded">Entrar</button>
+            <form action='/dashboard' className="d-flex flex-column justify-content-around p-2 bg-success rounded" style={{ width: "18rem", height: '14rem' }} onSubmit={e => formHandler(e)}>
+                <div className="d-flex flex-column">
+                    <input type="text" name="login" placeholder="Login" required className="p-2 m-2 rounded border-0 bg-light" />
+                    <input type="password" name="password" placeholder="Senha" required className="p-2 m-2 rounded border-0 bg-light" />
+                    {errorMessage ? <p className="text-danger-emphasis fw-bold">{errorMessage}</p> : null}
+                </div>
+                <button type="submit" className="mx-auto rounded border-0 text-light bg-dark p-2 fs-5 fw-bold rounded">Entrar</button>
             </form>
         </>
     )
